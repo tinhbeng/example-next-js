bd84a3c3b5a5f775df0866105969324286457b40
tinhbeng <tinhbeng1092@gmail.com>
2024-06-20 15:45:00 +0700

    full example

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -172,9 +172,6 @@ export function InvoicesTableSkeleton() {
             <InvoicesMobileSkeleton />
             <InvoicesMobileSkeleton />
             <InvoicesMobileSkeleton />
-            <InvoicesMobileSkeleton />
-            <InvoicesMobileSkeleton />
-            <InvoicesMobileSkeleton />
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
